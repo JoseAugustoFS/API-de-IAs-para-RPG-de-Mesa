c56646c0ed9f7a3a323b4dffdf23150e5b305a64
José Augusto <134980478+JoseAugustoFS@users.noreply.github.com>
2024-07-15 11:00:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask_cors import CORS, cross_origin
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-Stable_Diffusion_API_KEY = '9c9CNlAPRg8QJ7urqULNOiNIzbEZyBmh0nyq4GbsJBojvAwj2pRqLJerEIns'
+Stable_Diffusion_API_KEY = '4fAjYanqPNDZxogyDgt0yDEUJIYvsRFVAhlitV1Wwc3iUdKK9RH0yG8XXx1b'
 Google_AI_Studio_API_KEY = 'AIzaSyC6zxB47mEhvS2SIik7DEiTtmfWmlW7c6Q'
 
 genai.configure(api_key=Google_AI_Studio_API_KEY)
